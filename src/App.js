6bd96fc638843c94743bb54c552bbfd889f47b7b
Benji Mirschel <blueboi@Benjis-MacBook-Pro.local>
2019-07-02 15:24:39 -0400

    Finished creating basic store page to hold 4 items

--- src/App.js
+++ src/App.js
@@ -3,25 +3,20 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/home.component'
+import ShopPage from './pages/shop/shop.component.jsx';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS</h1>
-  </div>
-);
-  
 
 function App() {
   return (
     <div>
     <Switch>
       <Route exact path='/' component={HomePage} />
-      <Route path='/hats' component={HatsPage} />
+      <Route path='/shop' component={ShopPage} />
     </Switch>
     </div>
     // Component: Component we want to render. Path: When URL is at the base '/' render homepage. 
     // Exact is a bool property. It means path must be exatly '/' for homepage to render
-    //Switch will not render anything else after first path match
+    // Switch will not render anything else after first path match
   );
 }
 
