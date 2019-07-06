f5ad947289e516451d5adb431bf4ab1b112d1f86
Benji Mirschel <cliff.mirschel@gmail.com>
2019-07-06 12:02:22 -0400

    Implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx'
 import { auth, createUserProfileDocument} from './firebase/firebase.utils';
+
 class App extends React.Component {
   constructor(){
     super();
@@ -25,25 +26,22 @@ class App extends React.Component {
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
     if (userAuth) {
       const userRef = await createUserProfileDocument(userAuth);
-
+      
       userRef.onSnapshot(snapShot => {
         this.setState({
           currentUser: {
             id: snapShot.id,
             ...snapShot.data()
-            // Creating a new object that has all of the properties of our snapshot we want, as well as our unique id
           }
-        }, () =>{
-          // console.log(this.state);
-          //Pass a second param function (callback) in order to make sure state has been set before we check it in the
-          // console
-        })
+        });
+
+        console.log(this.state);
       });
     }
-    this.setState({currentUser: userAuth});
-      //console.log(user) to see the presisting user object provided by firebase
-    })
-  }
+
+    this.setState({ currentUser: userAuth });
+  });
+}
 
   componentWillUnmount(){
     this.unsubscribeFromAuth();
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import './sign-in-and-sign-up.styles.scss';
-import SignIn from '../../components/sign-in/sign-in.component.jsx'
+import SignIn from '../../components/sign-in/sign-in.component.jsx';
+import SignUp from '../../components/SignUp/SignUp';
 
 const SignInAndSignUpPage = () => (
     <div className='sign-in-and-sign-up'>
         <SignIn />
+        <SignUp />
     </div>
 );
 
