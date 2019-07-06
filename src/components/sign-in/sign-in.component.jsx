4c1d85830eef38587aa996774ecae940dfe08da1
Benji Mirschel <cliff.mirschel@gmail.com>
2019-07-06 12:08:44 -0400

    finished sign-in and sign-out components

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils'
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils'
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
@@ -16,10 +16,18 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
+    
+    const {email, password} = this.state;
 
-    this.setState({ email: '', password: '' });
+    try {
+      await auth.signInWithEmailAndPassword(email,password);
+      this.setState({ email: '', password: '' });
+    } catch (err){
+      console.error(err);
+    }
+
   };
 
   handleChange = event => {
