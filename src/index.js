6fdb198d098f05c47d764b834999b6f4c0fd9dba
Benji Mirschel <cliff.mirschel@gmail.com>
2019-07-08 21:07:40 -0400

    Added redux to our project

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { Provider} from 'react-redux';
+import Store from './redux/store';
 
 // Wrap our app in BrowserRouter which provides routing for project
 ReactDOM.render(
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>, document.getElementById('root'));
+    <Provider store={Store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+    , document.getElementById('root'));
