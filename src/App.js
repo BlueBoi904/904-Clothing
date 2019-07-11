9ad78b47be6f156beed7725c852414bc47807f9b
Benji Mirschel <cliff.mirschel@gmail.com>
2019-07-11 16:51:51 -0400

    added commet

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { auth, createUserProfileDocument} from './firebase/firebase.utils';
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.actions';
 
-
+//Hello
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
