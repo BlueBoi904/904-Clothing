91ec18a4436c9750f397a3d82b2cceea3ce8d088
Benji Mirschel <cliff.mirschel@gmail.com>
2019-07-11 21:53:41 -0400

    Setup redux for cart items

--- src/redux/cart/cart-actions.js
+++ src/redux/cart/cart-actions.js
@@ -3,3 +3,10 @@ import CartActionTypes from './cart.types';
 export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN
 });
+// Actions can have a type, and payload
+export const addItem = item => ({
+  type: CartActionTypes.ADD_ITEM,
+  payload: item
+});
+
+// Add items function gets the item, and generates a new action with the action type of ADD_ITEM.
