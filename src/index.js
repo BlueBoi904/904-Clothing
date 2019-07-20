17312ae21bfa62001b75c58eb8ccc774fa50e052
Benji Mirschel <cliff.mirschel@gmail.com>
2019-07-20 09:07:25 -0400

    Added session persistence using localStorage obj and redux-persist

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+// Persistor
+import { PersistGate } from "redux-persist/integration/react";
 
-import store from './redux/store';
+import { store, persistor } from "./redux/store";
 
-import './index.css';
-import App from './App';
-
+import "./index.css";
+import App from "./App";
+// Wrap our app in PersistGate, so our app always has access to persist flow itself
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,20 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
-
 import cartReducer from './cart/cart-reducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  // Reducers we want to persist are stored in the whitelist array
+  whitelist: ['cart']
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
