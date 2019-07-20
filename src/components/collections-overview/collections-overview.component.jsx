c1d4d035b287f54a87d5313dfb9b5657d7c0be6b
Benji Mirschel <cliff.mirschel@gmail.com>
2019-07-20 14:11:34 -0400

    Updated new redux flow

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import CollectionPreview from '../collection-preview/collection-preview';
 import {createStructuredSelector} from 'reselect';
 import "./collections-overview.styles.scss";
-import {selectCollections} from '../../redux/shop/shop.selectors'
+import {selectCollectionsForPreview} from '../../redux/shop/shop.selectors'
 
 const CollectionsOverview = ({ collections }) => (
   <div className="collections-overview">
@@ -14,7 +14,7 @@ const CollectionsOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
+    collections: selectCollectionsForPreview
 })
 
 export default connect(mapStateToProps)(CollectionsOverview);
