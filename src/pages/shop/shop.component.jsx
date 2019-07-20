08b2d09a223923a3f7b54c96ac70f05d2cf42b91
Benji Mirschel <cliff.mirschel@gmail.com>
2019-07-20 09:39:47 -0400

    restructured our shop component to use redux state

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
-import SHOP_DATA from  './shop.data.js';
 import CollectionPreview from '../../components/collection-preview/collection-preview';
+import {selectCollections} from '../../redux/shop/shop.selectors';
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            collections: SHOP_DATA,
-        }
-    }
-
-    render () {
-        const {collections} = this.state;
-        return (
+const ShopPage = ({collections}) => (
             <div className='shop-page'>
                 {
                     collections.map(({id, ...otherCollectionProps}) => (
@@ -21,8 +12,11 @@ class ShopPage extends React.Component {
                     ))
                 }
             </div>
-        )
-    }
-}
+);
 
-export default ShopPage;
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+})
+
+
+export default connect(mapStateToProps)(ShopPage);
